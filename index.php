43fef1a92188c152ec7b953b9a1f650671449509
Zachary Sousa <zs20np@snpsteam.com>
2024-02-29 12:56:27 -0500

    first

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+        <title>Henry to UTF-8</title>
+        <meta name="description" content=""/>
+        <meta name="author" content="Zachary Sousa"/>
+
+        <link href="style.css" rel="stylesheet" type="text/css"/>
+    </head>
+
+    <body>
+
+        <!-- webform -->
+
+        <?php
+            //make cypher
+            
+
+            //convert function
+        ?>
+
+    </body>
+
+</html>
