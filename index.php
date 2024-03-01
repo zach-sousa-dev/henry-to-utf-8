47c25e9a86075ad123b56df8119df3afaf3094f5
Z4CH4RYSNP <zs20np@snpsteam.com>
2024-02-29 23:19:55 -0500

    im so confused but like i think im close??

--- index.php
+++ index.php
@@ -46,31 +46,95 @@
 
             $bad_str = file_get_contents("example.txt");  
             //echo decode_str($bad_str, $to_utf_cypher);   
-            echo count($to_utf_cypher);    
-            echo count($to_henry_cypher);    
-            echo "<br><br><h3>TO HENRY</h3><br>";
+            echo "<h3>TO HENRY</h3>";
             var_dump($to_henry_cypher);
+            echo "<h3>TO UTF8</h3>";
+            var_dump($to_utf_cypher);
             echo "<br>";
             echo "<br>";
-            echo "<br>";
-            echo decode_str($bad_str, $to_utf_cypher);
+            //echo new_decode_str($bad_str, 3, $to_utf_cypher);
+            //echo (new_new_decode_str($bad_str, $to_utf_cypher));
+            echo mb_convert_encoding($bad_str, $to_utf_cypher, $to_henry_cypher);
 
             //convert function
 
-            function decode_str($input_str, $cypher) {
+            function old_decode_str($input_str, $cypher) {
                 $output_str = "";
                 $chars = str_split($input_str);
 
                 for($i = 0; $i < count($chars); $i++) {
                     if( array_key_exists($chars[$i], $cypher) ) {
-                        $output_str.=$cypher[$chars[$i]];
+                        $output_str .= $cypher[$chars[$i]];
                     } else {
-                        $output_str.=$chars[$i];
+                        $output_str .= $chars[$i];
                     }
                 }
 
                 return $output_str;
             }
+
+            function new_new_decode_str($input_str, $cypher) {
+                $output_str = "";
+                $chars = mb_str_split($input_str);
+
+                for($i = 0; $i < count($chars); $i++) {
+                    if( array_key_exists($chars[$i], $cypher) ) {
+                        $output_str .= $cypher[$chars[$i]];
+                    } else {
+                        $output_str .= $chars[$i];
+                    }
+                }
+
+                return $output_str;
+            }
+        ?>
+
+        <?php
+            function new_decode_str($input_str, $highest_len, $cypher) {
+                $input_arr = str_split($input_str);
+                $text_chunk = [];
+                $output_str = "";
+
+                $cypher_keys = array_keys($cypher);
+                for($i = 0; $i < count($cypher_keys); $i++) {
+                    echo "i am element of cypher_keys: " . $cypher_keys[$i];
+                }
+
+                $all_cypher_key_chars_str = "";
+                for($i = 0; $i < count($cypher_keys); $i++) {
+                    $all_cypher_key_chars_str .= $cypher_keys[$i];
+                }
+                $all_cypher_key_chars_arr = str_split($all_cypher_key_chars_str);
+
+                var_dump($all_cypher_key_chars_arr);
+                echo "<br>";
+
+                for($i = 0; $i < count($input_arr); $i++) {
+
+                    array_push($text_chunk, $input_arr[$i]);
+
+                    $concatenated = "";
+                    for($j = 0; $j < count($text_chunk); $j++) {
+                        $concatenated .= $text_chunk[$j];
+                    }
+
+                    if( array_key_exists($concatenated, $cypher) ) {//found valid character
+                        $output_str .= $cypher[$concatenated];
+                        //$i -= (count($text_chunk) - 2);
+                        $text_chunk = [];
+                    } else if(count($text_chunk) == $highest_len) { //no valid characters
+                        $i -= ($highest_len - 2);
+                        $text_chunk = [];
+                    } else {
+                        if( !in_array($input_arr[$i], $all_cypher_key_chars_arr) ) {
+                            $output_str .= $input_arr[$i];
+                        }
+                    }
+                }
+
+                return $output_str;
+            }
+
         ?>
 
     </body>
