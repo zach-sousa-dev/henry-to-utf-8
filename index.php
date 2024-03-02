57bad455b437a81b5945b498ab54fb9337fa9268
Zachary Sousa <zs20np@snpsteam.com>
2024-03-01 19:15:03 -0500

    working with trim() and also added a text area that does nothing yet

--- index.php
+++ index.php
@@ -14,8 +14,15 @@
 
         <!-- webform -->
 
+        <form>
+
+            <textarea id="text-input" name="text-input" placeholder="Input text to convert..."></textarea>
+
+        </form>
+
         <?php
-            //make cypher
+            //THIS SECTION CREATES THE CYPHER
+
             $cypher_filename = "cypher.txt";
             $cypher_file = file_get_contents($cypher_filename);
 
@@ -24,15 +31,17 @@
             H,e,n,r,y,F,o,n,t\n
             N,o,t,H,e,n,r,y,s
             */
-
-
             
             $henry_chars_str = explode("\n", $cypher_file)[0];
             $henry_chars_arr = explode(",", $henry_chars_str); 
             $utf_chars_str = explode("\n", $cypher_file)[1];
             $utf_chars_arr = explode(",", $utf_chars_str);
 
-            
+            foreach($henry_chars_arr as $index => $word)
+                $henry_chars_arr[$index] = trim($word);
+
+            foreach($utf_chars_arr as $index => $word)
+                $utf_chars_arr[$index] = trim($word);
 
             $to_utf_cypher = [];
             for($i = 0; $i < count($henry_chars_arr); $i++) {
@@ -43,41 +52,25 @@
             for($i = 0; $i < count($utf_chars_arr); $i++) {
                 $to_henry_cypher[$utf_chars_arr[$i]] = $henry_chars_arr[$i]??"ERROR!";
             }
-
-            echo "<h3>TO HENRY</h3>";
-            //var_dump($henry_chars_arr);
-            var_dump($to_henry_cypher);
-            echo "<h3>TO UTF8</h3>";
-            //var_dump($utf_chars_arr);
-            var_dump($to_utf_cypher);
             
         ?>
 
         <?php
 
             $bad_str = file_get_contents("example.txt");  
- 
-            
-            echo "<br>";
-            echo "<br>";
 
-            echo (new_new_decode_str($bad_str, $to_utf_cypher));
+            echo (decode_str($bad_str, $to_utf_cypher));
 
             //convert function
+            function decode_str($input_str, $cypher) {
+                $output_str = "";   //init output
+                $chars = preg_split("//u", $input_str);     //split text by unicode characters
 
-            function new_new_decode_str($input_str, $cypher) {
-                $output_str = "";
-                $chars = preg_split("//u", $input_str);
-
-                echo "<br><br><h3>INPUT</h3>";
-                var_dump($chars);
-                echo "<br>";
-
-                for($i = 0; $i < count($chars); $i++) {
+                for($i = 0; $i < count($chars); $i++) {     //look at each character
                     if( array_key_exists($chars[$i], $cypher) ) {
-                        $output_str .= $cypher[$chars[$i]];
+                        $output_str .= $cypher[$chars[$i]]; //concatenate decyphered character
                     } else {
-                        $output_str .= $chars[$i];
+                        $output_str .= $chars[$i];  //concatenate ignored character
                     }
                 }
 
